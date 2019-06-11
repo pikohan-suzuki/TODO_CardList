0185e6eda6220d1a7d74505300fbd8d1577a9764
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-06-11 21:37:56 +0900

    okボタンクリック時のインターフェースを追加

--- TODO/CardList/app/src/main/java/com/amebaownd/pikohan_nwiatori/todocardlist/Dialogs/NewCardDialog.kt
+++ TODO/CardList/app/src/main/java/com/amebaownd/pikohan_nwiatori/todocardlist/Dialogs/NewCardDialog.kt
@@ -1,10 +1,7 @@
 package com.amebaownd.pikohan_nwiatori.todocardlist.Dialogs
 
-import android.app.Activity
 import android.app.AlertDialog
 import android.app.Dialog
-import android.app.PendingIntent
-import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.text.Editable
@@ -12,35 +9,33 @@ import android.text.TextWatcher
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import com.amebaownd.pikohan_nwiatori.todocardlist.Data.Event
 import com.amebaownd.pikohan_nwiatori.todocardlist.R
 import kotlinx.android.synthetic.main.new_card_layout.*
-import java.text.SimpleDateFormat
 import java.util.*
 
-interface newCartDialogOkButton:EventListener{
+interface NewCartDialogOkButton:EventListener{
     fun onOkButtonClick()
 }
-class NewCardDialog():DialogFragment(){
+
+class NewCardDialog:DialogFragment(){
     private var event:Event? = null
+    private var listener : NewCartDialogOkButton?=null
+    private var addButton:Button? = null
+    private var titleEditText:EditText?=null
+    private var dateEditText:EditText?=null
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val alertDialog : View = activity!!.layoutInflater.inflate(R.layout.new_card_layout,null)
-        val addButton = alertDialog.findViewById<Button>(R.id.add_button)
-        val titleEditText= alertDialog.findViewById<EditText>(R.id.titleEditText)
-        val dateEditText= alertDialog.findViewById<EditText>(R.id.dateEditText)
+        addButton = alertDialog.findViewById<Button>(R.id.add_button)
+       titleEditText= alertDialog.findViewById<EditText>(R.id.titleEditText)
+        dateEditText= alertDialog.findViewById<EditText>(R.id.dateEditText)
         val memoEditText= alertDialog.findViewById<EditText>(R.id.memoEditText)
-        titleEditText.addTextChangedListener(editTextOnChangeListener)
-        dateEditText.addTextChangedListener(editTextOnChangeListener)
-        addButton.setOnClickListener{
-            event = Event(titleEditText.text.toString(),dateEditText.text.toString().toLong(),memoEditText.text.toString())
-            val intent= Intent()
-            val pendingIntent =activity!!.createPendingResult(targetRequestCode,intent, PendingIntent.FLAG_ONE_SHOT)
-            try{
-                pendingIntent.send(Activity.RESULT_OK)
-            }catch (ex: PendingIntent.CanceledException){
-                ex.printStackTrace()
-            }
+        titleEditText!!.addTextChangedListener(editTextOnChangeListener)
+        dateEditText!!.addTextChangedListener(editTextOnChangeListener)
+        addButton!!.setOnClickListener{
+            event = Event(titleEditText!!.text.toString(),dateEditText!!.text.toString().toLong(),memoEditText.text.toString())
+            listener!!.onOkButtonClick()
             this.dismiss()
         }
 
@@ -53,9 +48,12 @@ class NewCardDialog():DialogFragment(){
     fun getEvent():Event{
         return event!!
     }
+    fun setOnOkButtonClickListener(listener : NewCartDialogOkButton){
+        this.listener=listener
+    }
     private  val editTextOnChangeListener = object: TextWatcher{
         override fun afterTextChanged(p0: Editable?) {
-            add_button.isEnabled = titleEditText.text!=null && dateEditText!=null
+            addButton!!.isEnabled = titleEditText!!.text!=null && dateEditText!=null
         }
         override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
         }
